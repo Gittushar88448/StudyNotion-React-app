6f4daf93bcc5999d153a19f4dcc0062ace1aee74
Tushar <kmrtushar01@gmail.com>
2024-04-05 12:50:14 +0530

    adding routes of differnet pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar"
+import { useState } from "react";
 
 function App() {
+
+  const [isVisible , setIsVisible] = useState(false);
+
   return (
     <div className="App">
 
+      <Navbar isVisible={isVisible} setIsVisible={setIsVisible}/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<LogIn/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
     </div>
   );
 }
