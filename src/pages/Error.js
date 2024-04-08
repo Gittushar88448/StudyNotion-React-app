32397d047ad2c0f491790344e5d9138c07bade33
Tushar <kmrtushar01@gmail.com>
2024-04-08 19:11:17 +0530

    adding error 404 not found window

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='flex justify-center items-center text-white text-3xl h-full'>Error 404 Not Found</div>
+  )
+}
+
+export default Error
