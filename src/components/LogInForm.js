db12c8b1a9ab6514c5bd74c86ccee5f292e2567b
Tushar <kmrtushar01@gmail.com>
2024-04-10 21:16:59 +0530

    adding tailwind css

--- src/components/LogInForm.js
+++ src/components/LogInForm.js
@@ -28,10 +28,13 @@ const LogInForm = ({setIsVisible}) => {
     }
     
   return (
-    <div>
-        <form onSubmit={submitHandler}>
+    <div >
+        <form onSubmit={submitHandler} className='flex flex-col w-full gap-y-4 mt-6'>
             <label>
-                Email Address<sup>*</sup>
+                <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]'>
+                  Email Address
+                  <sup className='text-pink-200'>*</sup>
+                </p>
                 <input 
                 required
                 type='text'
@@ -39,11 +42,15 @@ const LogInForm = ({setIsVisible}) => {
                 name='email'
                 value={formData.email}
                 onChange={changeHandler}
+                className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'
                 />
             </label>
 
-            <label>
-                Password<sup>*</sup>
+            <label className='relative'>
+                <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375rem]'>
+                   Password
+                   <sup className='text-pink-200'>*</sup>
+                </p>   
                 <input 
                 required
                 type= {showPassword ? ("text") : ("password")}
@@ -51,21 +58,24 @@ const LogInForm = ({setIsVisible}) => {
                 name='password'
                 value={formData.password}
                 onChange={changeHandler}
+                className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'
                 />
                 
-                <span onClick={() => setShowPassword(prev => !prev)}>
+                <span onClick={() => setShowPassword(prev => !prev)} className='absolute right-3 top-[38px] cursor-pointer'>
 
-                    {showPassword ? (<AiOutlineEye/>) : (<AiOutlineEyeInvisible/>)}
+                    {showPassword ? (<AiOutlineEye fontSize={24} fill='#AFB2BF'/>) : (<AiOutlineEyeInvisible fontSize={24} fill='#AFB2BF'/>)}
 
                 </span>
 
-                <Link to="#">
-                    Forgot Password
+                <Link to="#" >
+                    <p className='text-xs mt-1 text-blue-100 max-w-max ml-auto '>
+                       Forgot Password
+                    </p>
                 </Link>
 
             </label>
 
-                <button>
+                <button className='bg-yellow-50 rounded-[8px] font-medium text-richblack-900 px-[12px] py-[8px] mt-6'>
                     Sign In
                 </button>
 
